16add4281881b9227ec7af5afb066120034dba2e
ofemjohn <ofemjohn@gmail.com>
2023-03-21 00:53:31 +0100

    spread

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,2 +1,3 @@
-const concatArrays = (array1, array2, string) => [].concat(...array1, ...array2, ...string)
-module.exports = concatArrays
+const concatArrays = (array1, array2, string) => [].concat(...array1, ...array2, ...string);
+
+module.exports = concatArrays;
