ef699bf1d82c847a83f6994144f5bbad24868708
ofemjohn <ofemjohn@gmail.com>
2023-03-23 13:53:37 +0100

    promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 const getFullResponseFromAPI = (trueORfalse) => new Promise((resolve, reject) => {
   if (trueORfalse) resolve({ status: 200, body: 'success' });
-  else reject(Error('the fake Api is not working properly'));
+  reject(Error('the fake Api is not working properly'));
 });
 
 export default getFullResponseFromAPI;
