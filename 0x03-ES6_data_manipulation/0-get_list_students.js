d339588209b4b108a0cbb8c40d09a074b560e593
ofemjohn <ofemjohn@gmail.com>
2023-03-28 12:54:43 +0100

    create an array object in a function

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisc',
+    },
+
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisc',
+    },
+  ];
+}
+
+export default getListStudents;
