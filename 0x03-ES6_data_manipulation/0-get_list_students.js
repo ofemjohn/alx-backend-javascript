56d102b72cfd19cc8fd8ace4cd76aece4f0c4bee
ofemjohn <ofemjohn@gmail.com>
2023-03-28 13:03:10 +0100

    changed the array list function to arrow function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,21 @@
-function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisc',
-    },
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisc',
+  },
 
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
 
-    {
-      id: 3,
-      firstName: 'Serena',
-      location: 'San Francisc',
-    },
-  ];
-}
+  {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisc',
+  },
+];
 
 export default getListStudents;
