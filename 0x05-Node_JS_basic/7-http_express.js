47b5485bd655fcdcc52230a2158f97cd7e361e94
ofemjohn <ofemjohn@gmail.com>
2023-05-16 23:08:28 +0100

    recreate the small HTTP server using Express:

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,24 +1,28 @@
 const express = require('express');
 
-const app = express();
-
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = process.argv[2];
+const DATABASE = args[0];
+
+const port = 1245;
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.send('This is the list of our students\n');
-  let students = [];
-  countStudents(DATABASE).then((data) => {
-    students = data;
-  });
-  res.end(students.join('\n'));
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
 });
 
-app.listen(1245);
+app.listen(port, () => {
+});
 
 module.exports = app;
