389aac3fa9eb8193116eb30766a05e46fc943c72
ofemjohn <ofemjohn@gmail.com>
2023-05-16 23:18:00 +0100

    recreate the small HTTP server using Express:

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const { countStudents, readDatabase } = require('./3-read_file_async');
 
-const app = express();
 const args = process.argv.slice(2);
-const DATABASE = args[0];
+const countStudents = require('./3-read_file_async');
 
+const DATABASE = args[0];
+const port = 1245;
+const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
 app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
   try {
-    const { lines, database } = await readDatabase(DATABASE);
-    res.send(`This is the list of our students\nNumber of students: ${countStudents(lines)}\nNumber of students in CS: ${database.CS.length}. List: ${database.CS.join(', ')}\nNumber of students in SWE: ${database.SWE.length}. List: ${database.SWE.join(', ')}\n`);
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
-    res.status(500).send('Internal Server Error');
+    res.send(`${msg}${error.message}`);
   }
 });
-
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
 });
-
 module.exports = app;
