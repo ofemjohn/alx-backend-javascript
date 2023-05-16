5247f6f8d4dfab05fd37bd5160ac879c4d5b3523
ofemjohn <ofemjohn@gmail.com>
2023-05-16 13:23:17 +0100

    used process.stdout.write instead of readlines

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,24 @@
-const readline = require('readline');
+const welcomeMessage = 'Welcome to Holberton School, what is your name?\n';
 
-const read = readline.createInterface({
-  input: process.stdin,
+process.stdout.write(welcomeMessage);
+
+process.stdin.setEncoding('utf8');
+
+const readline = () => {
+  const read = process.stdin.read();
+  return read !== null ? read : '';
+};
+
+const readname = (name) => {
+  process.stdout.write(`Your name is: ${name}\n`);
+};
+
+const end = () => {
+  process.stdout.write('This important software is now closing\n');
+};
+
+process.stdin.on('readable', () => {
+  const name = readline();
+  readname(name);
+  end();
 });
-
-read.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
-});
-
-read.on('close', () => {
-  console.log('This important software is now closing');
-});
-
-console.log('Welcome to Holberton School, what is your name?');
