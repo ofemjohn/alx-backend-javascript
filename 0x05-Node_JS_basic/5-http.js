2cbabfac370ef21092ef436171068c6b2fb53554
ofemjohn <ofemjohn@gmail.com>
2023-05-16 23:49:58 +0100

    corrected the file that create a small HTTP server using the http module:

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const args = process.argv.slice(2);
+const argm = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
+const DB = argm[0];
 
+const host = '127.0.0.1';
 const port = 1245;
-const hostname = '127.0.0.1';
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
@@ -19,7 +19,7 @@ const app = http.createServer(async (req, res) => {
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(DATABASE);
+      const students = await countStudents(DB);
       res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
@@ -29,9 +29,8 @@ const app = http.createServer(async (req, res) => {
   res.end();
 });
 
-app.listen(port, hostname, () => {
+app.listen(port, host, () => {
   //   console.log(`Server running at http://${hostname}:${port}/`);
-
 });
 
 module.exports = app;
