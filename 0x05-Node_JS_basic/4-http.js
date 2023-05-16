fd6ca2b6cb81af2fe6152294bd54fb42e1681cb9
ofemjohn <ofemjohn@gmail.com>
2023-05-16 22:01:48 +0100

    created a simple http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+  });
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+app.listen(1245, '127.0.0.1', () => {
+  console.log('Server running at http://127.0.0.1:1245/');
 });
 
 module.exports = app;
