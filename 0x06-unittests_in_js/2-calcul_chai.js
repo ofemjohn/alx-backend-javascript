d5bd46f101a4c8a3e91ef3c3e8b8e4615e9965f8
ofemjohn <ofemjohn@gmail.com>
2023-05-18 23:52:59 +0100

    correction

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,24 +1,28 @@
 function calculateNumber(type, a, b) {
-  const num1 = Number(a);
-  const num2 = Number(b);
-
-  if (isNaN(num1) || isNaN(num2)) {
-    throw new TypeError();
+  // Validate the type argument.
+  if (!['SUM', 'SUBTRACT', 'DIVIDE'].includes(type)) {
+    throw new Error('Invalid operation type.');
   }
 
+  // Convert the arguments to numbers.
+  const numOne = Number(a);
+  const numTwo = Number(b);
+
+  // Perform the operation.
   switch (type) {
-    case "SUM":
-      return Math.round(num1) + Math.round(num2);
-    case "SUBTRACT":
-      return Math.round(num1) - Math.round(num2);
-    case "DIVIDE":
-      if (num2 === 0) {
-        return "Error";
+    case 'SUM':
+      return numOne + numTwo;
+    case 'SUBTRACT':
+      return numOne - numTwo;
+    case 'DIVIDE':
+      if (numTwo === 0) {
+        return 'Error';
       }
-      return Math.round(num1) / Math.round(num2);
+      return numOne / numTwo;
     default:
-      throw new TypeError();
+      throw new Error('Invalid operation type.');
   }
 }
 
 module.exports = calculateNumber;
+
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,51 +1,36 @@
-import chai from 'chai';
-import expect from 'chai';
-import calculateNumber from './2-calcul_chai';
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
-describe('test calculate number', () => {
-  it('checks the rounded sum', () => {
-    // Tests that the `calculateNumber` function returns the correct sum for two numbers.
+describe('#SUM operation', () => {
+  it('should return the sum of its rounded arguments', () => {
     expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-    expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
     expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
-    expect(calculateNumber('SUM', -1, 3)).to.equal(2);
-    expect(calculateNumber('SUM', -1, -4)).to.equal(-5);
-    expect(calculateNumber('SUM', -1.2, -1.4)).to.equal(-2);
-    expect(calculateNumber('SUM', -2, 3.2)).to.equal(1);
   });
+});
 
-  it('checks the rounded difference', () => {
-    // Tests that the `calculateNumber` function returns the correct difference for two numbers.
+describe('#SUBTRACT operation', () => {
+  it('should return the subtraction of its rounded arguments', () => {
     expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
-    expect(calculateNumber('SUBTRACT', 3.3, 1)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', 3, 2)).to.equal(1);
     expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
-    expect(calculateNumber('SUBTRACT', -1, 3)).to.equal(-4);
-    expect(calculateNumber('SUBTRACT', -1, -4)).to.equal(3);
-    expect(calculateNumber('SUBTRACT', -1.2, -1.4)).to.equal(0);
-    expect(calculateNumber('SUBTRACT', -2, 3.2)).to.equal(-5);
+    expect(calculateNumber('SUBTRACT', 3.7, 1.5)).to.equal(2);
   });
+});
 
-  it('checks the rounded division', () => {
-    // Tests that the `calculateNumber` function returns the correct quotient for two numbers.
-    expect(calculateNumber('DIVIDE', 6, 2)).to.equal(3);
-    expect(calculateNumber('DIVIDE', 3.3, 1)).to.equal(3);
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    expect(calculateNumber('DIVIDE', 1.7, 3.7)).to.equal(0.5);
-    expect(calculateNumber('DIVIDE', -6, 3)).to.equal(-2);
-    expect(calculateNumber('DIVIDE', -6, -3)).to.equal(2);
-    expect(calculateNumber('DIVIDE', -6.2, -3.4)).to.equal(2);
-    expect(calculateNumber('DIVIDE', -2, 2.1)).to.equal(-1);
-    expect(calculateNumber('DIVIDE', -2, 0)).to.equal('Error');
+describe('#DIVIDE operation', () => {
+  it('should return the quotient from the division its rounded arguments', () => {
+    expect(calculateNumber('DIVIDE', 3, 1)).to.equal(3);
+    expect(calculateNumber('DIVIDE', 3.1, 1.5)).to.equal(1.5);
+    expect(calculateNumber('DIVIDE', 6.5, 1.5)).to.equal(3.5);
+    expect(calculateNumber('DIVIDE', -3.7, -1.5)).to.equal(4);
+    expect(calculateNumber('DIVIDE', 3, 0)).to.equal('Error');
   });
+});
 
-
-  it('checks the NaN number', () => {
-    // Tests that the `calculateNumber` function throws an error when passed a NaN value.
-    expect(() => calculateNumber('SUM', NaN, 5)).to.throw('NaN');
-    expect(() => calculateNumber('SUBTRACT', NaN, 5)).to.throw('NaN');
-    expect(() => calculateNumber('DIVIDE', NaN, 5)).to.throw('NaN');
-    expect(() => calculateNumber('shhh', NaN, 5)).to.throw('Invalid operation');
+describe('#INVALID operation', () => {
+  it('should throw an exception for invalid type', () => {
+    expect(() => calculateNumber.bind('MULTIPLY', 1, 3)).to.throw();
+    expect(() => calculateNumber.bind(false, 1, 2)).to.throw();
   });
 });
