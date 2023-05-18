a10eb50b896bbd81edbf1f6983e6ecb7c6d601f5
ofemjohn <ofemjohn@gmail.com>
2023-05-18 22:39:58 +0100

    corrected test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,67 +1,36 @@
+// adding 2 numbers
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe("calculateNumber function:", function() {
-  
-    describe("Add one float & one integer:", function() {
-        it('Returns 5.', function() {
-            assert.strictEqual(calculateNumber(1, 3.7), 5);
-        });
+describe('calculateNumber test suite', function() {
+  describe('Add positive numbers', function() {
+    it('should return 4', function() {
+      assert.equal(calculateNumber(1, 3), 4);
     });
-
-    describe("Add one float & one int reversed:", function () {
-        it('Returns 5.', function () {
-            assert.strictEqual(calculateNumber(3.7, 1), 5);
-        });
+    it('should return 5', function() {
+      assert.equal(calculateNumber(1, 3.7), 5);
     });
-
-    describe("Calculates two floats:", function() {
-        it('Returns 5.', function() {
-            assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        });
+    it('should return 5', function() {
+      assert.equal(calculateNumber(1.2, 3.7), 5);
     });
-
-    describe("Add two negative integers:", function() {
-        it('Returns -6.', function() {
-            assert.strictEqual(calculateNumber(-2, -4), -6);
-        });
+    it('should return 6', function() {
+      assert.equal(calculateNumber(1.5, 3.7), 6);
     });
-
-    describe("Add positive and negative float:", function() {
-        it('Returns 2.', function() {
-            assert.strictEqual(calculateNumber(3.5, -1.5), 2);
-        });
+  });
+  describe('Add negative numbers', function() {
+    it('should return -5', function() {
+      assert.equal(calculateNumber(-1.6, -3), -5);
     });
-
-    describe("Add two large integers:", function() {
-        it('Returns 200000000.', function() {
-            assert.strictEqual(calculateNumber(100000000, 100000000), 200000000);
-        });
+    it('should return -6', function() {
+      assert.equal(calculateNumber(-1.6, -3.6), -6);
     });
-
-    describe("Add zero and a float:", function() {
-        it('Returns 4.5.', function() {
-            assert.strictEqual(calculateNumber(0, 4.5), 4.5);
-        });
+  });
+  describe('Add positive and negative numbers', function() {
+    it('should return 2', function() {
+      assert.equal(calculateNumber(-1, 3), 2);
     });
-
-    describe("add two integers:", function() {
-        it('Returns 4.', function() {
-            assert.strictEqual(calculateNumber(1, 3), 4);
-        });
+    it('should return 1', function() {
+      assert.equal(calculateNumber(-1.6, 3), 1);
     });
-
-    describe("Add two negative floats:", function() {
-        it('Returns -3.', function() {
-            assert.strictEqual(calculateNumber(-1.5, -1.5), -3);
-        });
-    });
-
-    describe("Add one float & one integer (round down):", function() {
-        it('Returns 4.', function() {
-            assert.strictEqual(calculateNumber(1, 3.3), 4);
-        });
-    });
-
+  });
 });
-
