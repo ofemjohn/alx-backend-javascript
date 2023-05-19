7042f57578606f7bfb41d4861eec573685db97cf
ofemjohn <ofemjohn@gmail.com>
2023-05-19 02:23:24 +0100

    added a new end point and a package.json file

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,22 +1,16 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  const cartId = parseInt(req.params.id);
-  if (isNaN(cartId)) {
-    res.status(400).send('Invalid cart ID');
-  } else {
-    res.send(`Payment methods for cart ${cartId}`);
-  }
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
 app.listen(7865, () => {
-  console.log('Server is live at http://localhost:7865');
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,40 +1,40 @@
+const request = require('request');
 const { expect } = require('chai');
-const request = require('supertest');
-const app = require('./api');
 
-describe('Express app test suite', () => {
-  it('should return home page', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Welcome to the payment system');
+describe('Integration Testing', () => {
+  const baseURL = 'http://localhost:7865';
+
+  describe('GET /', () => {
+    it('Returns StatusCode: 200 | Body: Welcome to the payment system', (done) => {
+      request.get(baseURL, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
         done();
       });
+    });
   });
 
-  describe('Cart page', () => {
-    it('should return payment methods for a valid cart ID', (done) => {
-      request(app)
-        .get('/cart/123')
-        .expect(200)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.text).to.equal('Payment methods for cart 123');
+  describe('GET /cart/:id', () => {
+    const validCartIds = ['12', '1', '123'];
+    const invalidCartIds = ['a12', 'a12b', '12b', 'hello', ''];
+
+    validCartIds.forEach((cartId) => {
+      it(`Responds with 200 and id ${cartId} in msg`, (done) => {
+        request.get(`${baseURL}/cart/${cartId}`, (error, response, body) => {
+          expect(response.statusCode).to.equal(200);
+          expect(body).to.equal(`Payment methods for cart ${cartId}`);
           done();
         });
+      });
     });
 
-    it('should return 400 status code for invalid cart ID', (done) => {
-      request(app)
-        .get('/cart/abc')
-        .expect(400)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.text).to.equal('Invalid cart ID');
+    invalidCartIds.forEach((cartId) => {
+      it(`Responds with 404 for invalid cart ID: ${cartId}`, (done) => {
+        request.get(`${baseURL}/cart/${cartId}`, (error, response, body) => {
+          expect(response.statusCode).to.equal(404);
           done();
         });
+      });
     });
   });
 });
